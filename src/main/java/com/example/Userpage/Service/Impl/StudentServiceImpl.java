9a87651a72a0014d9be29ba5277ff8c64e83232e
Sunil <sunilchand156@gmail.com>
2023-03-22 18:15:34 +0530

    student

---
+++ src/main/java/com/example/Userpage/Service/Impl/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.Userpage.Service.Impl;
+
+import com.example.Userpage.Entity.Student;
+import com.example.Userpage.Repository.StudentRepository;
+import com.example.Userpage.Service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+}
